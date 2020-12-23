9b59370d7f1865a87cb9183e0fa43786f0fa8da4
Nabil Kazerouni <nabilkaz@gmail.com>
2020-12-22 23:00:45 -0800

    Add fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,11 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    fontFamily: {
+      sans: ['PT Sans', 'sans-serif'],
+      serif: ['PT Serif', 'serif'],
+      mono: ['PT Mono', 'mono'],
+    },
     extend: {},
   },
   variants: {
