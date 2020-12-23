9f00fbb7fca170011ae7e32430ebeb5d97ac0076
Nabil Kazerouni <nabilkaz@gmail.com>
2020-12-22 23:01:57 -0800

    Add PostItem to list

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 import React from "react";
-import {graphql, Link } from "gatsby";
-import TopNav from "../components/header";
+import {graphql } from "gatsby";
+import Nav from "../components/Nav";
+import PostItem from "../components/PostItem";
 
 export const query = graphql`
 query {
@@ -21,16 +22,9 @@ query {
 
 export default ({data: {posts}}) => (
   <div>
-    <TopNav />
-    <h1>Evbeff's blog</h1>
+    <Nav />
     {posts.nodes.map(post => (
-      <div key={`post-${post.slug}`}>
-        <h1>
-          <Link to={`/${post.slug}`}>
-            {post.title}
-          </Link>
-        </h1>
-      </div>
+      <PostItem key={`post-${post.slug}`} post={post} />
     ))}
   </div>
 );
