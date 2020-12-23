fe49f39e42fbf48d4ad91dc125f7b94d305d9f75
Nabil Kazerouni <nabilkaz@gmail.com>
2020-12-23 12:20:35 -0800

    Add layout

--- src/templates/post-template.js
+++ src/templates/post-template.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { graphql } from 'gatsby';
 import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
+import Layout from '../components/Layout';
 
 export const query = graphql`
     query ($slug: String!) {
@@ -16,13 +17,13 @@ export const query = graphql`
 `
 
 const postTemplate = ({data: {post}}) => (
-    <div>
+    <Layout>
         <h1>{post.title}</h1>
         <p>{post.date}</p>
         <div>
             {documentToReactComponents(JSON.parse(post.content.raw))}
         </div>
-    </div>
+    </Layout>
 )
 
 export default postTemplate;
