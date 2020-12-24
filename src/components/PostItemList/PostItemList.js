40c043fe64c0246cee43fd5d60af76fc1709395c
Nabil Kazerouni <nabilkaz@gmail.com>
2020-12-24 09:50:00 -0800

    Add sort for postlist

--- src/components/PostItemList/PostItemList.js
+++ src/components/PostItemList/PostItemList.js
@@ -3,7 +3,14 @@ import PostItem from '../PostItem'
 
 export default ({ posts }) => (
     <div className="flex flex-col max-w-prose mx-auto justify-content">
-      {posts.nodes.map(post => (
+      {posts.nodes
+      .slice()
+      .sort((a,b) => {
+        let aDate = new Date(a.date);
+        let bDate = new Date(b.date);
+        return  bDate - aDate
+      })
+      .map(post => (
         <PostItem key={`post-${post.slug}`} post={post} />
       ))}
     </div>
