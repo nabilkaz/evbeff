1aee20d7f29e5e4073442da28798361b86a343d5
Nabil Kazerouni <nabilkaz@gmail.com>
2020-12-24 13:59:21 -0800

    Add pizza favicon

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -12,6 +12,7 @@ export default function Layout({children}) {
                 <html lang="en" />
                 <title>{title}</title>
                 <meta name="description" content={description} />
+                <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🍕</text></svg>" />
             </Helmet>
             <div className="max-w-prose mx-auto items-center">
                 <Nav />
