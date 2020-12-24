7ef15712f228ad56502e1a2cae8c7e50ac762a97
Nabil Kazerouni <nabilkaz@gmail.com>
2020-12-24 13:46:13 -0800

    Add site metadata

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,11 +1,24 @@
 import React from "react";
 import Nav from "../Nav";
+import Helmet from 'react-helmet'
+import useSiteMetaData from '../hooks/use-sitemetadata'
 
 export default function Layout({children}) {
+    const {title, description} = useSiteMetaData();
+
     return (
-        <div className="max-w-prose mx-auto items-center">
-            <Nav />
-            {children}
-        </div>
+        <>
+            <Helmet>
+                <html lang="en" />
+                <title>{title}</title>
+                <meta name="description" content={description} />
+            </Helmet>
+            <div className="max-w-prose mx-auto items-center">
+                <Nav />
+                <main>
+                    {children}
+                </main>
+            </div>
+        </>
     )
 }
